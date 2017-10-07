1ec9129952077d8ea1ecdd9fdf4ff612eb5b632b
alexandrain <alexandrain90808@hotmail.fr>
2017-10-07 01:41:41 -0400

    1

---
+++ Assets/Tuyau_Tournant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuyau_Tournant : MonoBehaviour {
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.transform.Rotate (Vector3.forward, 90);
+	}
+}
